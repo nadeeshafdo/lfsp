82068a4e86c057b6d9746adcf0a0394374660092
Nadeesha Fernando <nadeeshafernando2020@gmail.com>
2024-12-19 02:37:03 +0530

    Initial commit

---
+++ src/main/java/com/nadeeshafdo/lfsp/LfspApplication.java
@@ -0,0 +1,13 @@
+package com.nadeeshafdo.lfsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LfspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LfspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nadeeshafdo/lfsp/LfspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadeeshafdo.lfsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LfspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
